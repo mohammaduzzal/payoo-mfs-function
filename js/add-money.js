4afbf7369e0b58af3df8cffce30f7fb2a9ece9ed
mohammaduzzal <aalp72142@gmail.com>
2024-09-20 16:30:54 +0600

    added the validation to the addMoney and cashOut

--- js/add-money.js
+++ js/add-money.js
@@ -6,10 +6,25 @@ document.getElementById('add-money-btn')
     const addMoney = getInputFieldValueById('amount-input');
     const pinNumber = getInputFieldValueById('pin-input')
 
+
+    if(isNaN(addMoney)){
+        alert('failed to add money')
+        return;
+    }
+
     if(pinNumber === 1234){
         const balance = getTextFieldValueById('present-balance');
         const newBalance = balance + addMoney;
         document.getElementById('present-balance').innerText = newBalance
+
+        // add to the transaction history
+        const p = document.createElement('p')
+        p.innerText = `Added: ${addMoney} tk. New Balance: ${newBalance}`
+
+        // common function would good for appendChild
+        document.getElementById('transaction-container').appendChild(p)
+
+
     }
     else{
         alert('wrong pin baby.think smarter.take rest and try again')
--- js/cash-out.js
+++ js/cash-out.js
@@ -5,10 +5,31 @@ document.getElementById('cash-out-btn')
     const amountCashInput = getInputFieldValueById('amount-cash-input');
     const pinCashInput = getInputFieldValueById('pin-cash-input');
 
+
+    if(isNaN(amountCashInput)){
+        alert('we only understand money baby')
+        return;
+    }
+
     if(pinCashInput === 1234){
         const remainBalance = getTextFieldValueById('present-balance');
+
+
+        if(amountCashInput > remainBalance){
+            alert('you dont have enough money baby')
+            return;
+        }
         const newBalanceRemain = remainBalance - amountCashInput
         document.getElementById('present-balance').innerText = newBalanceRemain
+
+        // add to the transaction history
+        const div = document.createElement('div');
+        div.classList.add('bg-yellow-300');
+        div.innerHTML = `
+        <h4 class="font-bold text-2xl">Cash Out</h4>
+         <p>${amountCashInput} tk withdraw. new balance ${newBalanceRemain}</p>
+        `
+        document.getElementById('transaction-container').appendChild(div)
 
     }
     else{
